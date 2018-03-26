e6b809f32f0dbd21c76cacad0eff4cbabed8fa6c
simon <stelter@tzi.de>
2018-03-26 13:10:02 +0200

    updated nav p simulator

--- iai_donbot_sim/scripts/navp_faker.py
+++ iai_donbot_sim/scripts/navp_faker.py
@@ -3,6 +3,7 @@
 import numpy as np
 import actionlib
 import rospy
+from actionlib_msgs.msg import GoalStatus
 from control_msgs.msg import FollowJointTrajectoryAction, JointTrajectoryControllerState, FollowJointTrajectoryGoal
 from geometry_msgs.msg import PoseStamped
 from move_base_msgs.msg import MoveBaseAction, MoveBaseResult, MoveBaseGoal
@@ -30,8 +31,10 @@ class TfWrapper(object):
         except ExtrapolationException as e:
             rospy.logwarn(e)
 
+
 class NavpFaker(object):
     def __init__(self):
+        name = 'nav_pcontroller/move_base'
         self.max_vel = 0.5
         self.freq = 0.2
         self.tf = TfWrapper()
@@ -39,15 +42,20 @@ class NavpFaker(object):
                                                    FollowJointTrajectoryAction)
         self.client.wait_for_server()
 
-        self.server = actionlib.SimpleActionServer('nav_pcontroller/move_base', MoveBaseAction,
+        self.server = actionlib.SimpleActionServer(name, MoveBaseAction,
                                                    execute_cb=self.cb, auto_start=False)
+        self.server.register_preempt_callback(self.preempt_cb)
         self.simple_goal_sub = rospy.Subscriber('move_base_simple/goal', PoseStamped, self.simple_goal_cb)
         self.server.start()
+        self.self_client = actionlib.SimpleActionClient(name, MoveBaseAction)
 
     def simple_goal_cb(self, goal_pose):
         g = MoveBaseGoal()
         g.target_pose = goal_pose
-        self.cb(g)
+        self.self_client.send_goal(g)
+
+    def preempt_cb(self):
+        self.client.cancel_goal()
 
     def cb(self, move_base_goal):
         move_base_goal = self.tf.transformPose('map', move_base_goal.target_pose)
@@ -72,15 +80,14 @@ class NavpFaker(object):
         def next_p(goal_p, last_p, f):
             diff = (goal_p - last_p)
             if diff > 0:
-                diff = min(diff, self.max_vel*f)
+                diff = min(diff, self.max_vel * f)
             else:
-                diff = max(diff, -self.max_vel*f)
+                diff = max(diff, -self.max_vel * f)
             return last_p + (diff)
 
         while not np.allclose(np.array(goal_js), p.positions[:3]):
-
             p = JointTrajectoryPoint()
-            p.time_from_start = last_p.time_from_start + rospy.Duration(1*self.freq)
+            p.time_from_start = last_p.time_from_start + rospy.Duration(1 * self.freq)
             p.positions = list(current_js.actual.positions)
 
             p.positions[0] = next_p(goal_js[0], last_p.positions[0], self.freq)
@@ -90,12 +97,22 @@ class NavpFaker(object):
             goal.trajectory.points.append(p)
             last_p = p
 
-        self.client.send_goal_and_wait(goal)
-        self.server.set_succeeded(MoveBaseResult())
+        self.client.send_goal(goal)
+        while not self.client.wait_for_result(rospy.Duration(.1)):
+            if self.server.is_preempt_requested():
+                rospy.loginfo('new goal, cancel old one')
+                self.client.cancel_all_goals()
+                self.server.set_preempted(MoveBaseResult())
+                break
+        result = self.client.get_result()
+        if result.error_code == result.SUCCESSFUL:
+            self.server.set_succeeded(MoveBaseResult())
+        else:
+            self.server.set_preempted(MoveBaseResult())
 
 
 if __name__ == '__main__':
-    rospy.init_node('navp_faker')
+    rospy.init_node('nav_p')
     muh = NavpFaker()
     rospy.loginfo('navp faker running.....')
     rospy.spin()
